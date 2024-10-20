b86eabcf4e7c60b33181eabfb038ceac917225ee
unknown <alwalidakash@gmail.com>
2024-10-21 00:34:24 +0200

    2nd commit

---
+++ src/components/ScearchFeed.jsx
@@ -0,0 +1,7 @@
+import React from "react";  
+const ScearchFeed = () =>{
+  return(
+    <div></div>
+  )
+}
+export default ScearchFeed;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Navber} from "./Navber";
+export {default as Feed} from "./Feed";
+export {default as ChannelDetails} from "./ChannelDetails";
+export {default as ScearchFeed} from "./ScearchFeed";
+export {default as VideoDetail} from "./VideoDetail";
